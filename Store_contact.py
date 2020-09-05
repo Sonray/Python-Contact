85d0c85d9668ef06a80c972c8f8106012fa06d32
David Okwacha <osweredavid@gmail.com>
2020-09-05 14:09:44 +0300

    Created user inputs for the contact positions

--- Store_contact.py
+++ Store_contact.py
@@ -6,3 +6,6 @@ class User_Contact:
         self.User_name  = User_name
         self.Account_password = Account_password
 
+Name_of_the_account = input('Enter the Account name eg Twitter,Facebook, etc')
+Name_of_the_user  = input('Enter your username')
+User_password = input('Enter the accounts' 'password')
