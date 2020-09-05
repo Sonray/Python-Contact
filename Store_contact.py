3c7e7a81b4bddf504b78c21a1fb92d49e430d9ff
David Okwacha <osweredavid@gmail.com>
2020-09-05 13:46:36 +0300

    Created user contact class

---
+++ Store_contact.py
@@ -0,0 +1,8 @@
+
+class User_Contact:
+
+    def __init__(self, Account_name, User_name, Account_password):
+        self.Account_name = Account_name
+        self.User_name  = User_name
+        self.Account_password = Account_password
+
