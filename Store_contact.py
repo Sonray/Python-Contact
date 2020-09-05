4ec0fbc68595778722c0b28a0feac5e1e7be3d89
David Okwacha <osweredavid@gmail.com>
2020-09-05 14:51:05 +0300

    Got user input and printed it out

--- Store_contact.py
+++ Store_contact.py
@@ -8,9 +8,14 @@ class User_Contact:
         self.Account_password = Account_password
 
     def Account_credentials(self):
-        return '{} {}'.format(self.Account_name, self.User_name, self.Account_password)
+        return '{} {} {}'.format(self.Account_name, self.User_name, self.Account_password)
 
 Name_of_the_account = input('Enter the Account name eg Twitter,Facebook, etc')
 Name_of_the_user  = input('Enter your username')
 User_password = input('Enter the accounts' 'password')
+
+user = User_Contact(Name_of_the_account, Name_of_the_user,User_password)
+
+print(user.Account_credentials())
+
 
