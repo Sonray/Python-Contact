0cdb7af46dad7d6e40797384eb8849a087b8a7c1
David Okwacha <osweredavid@gmail.com>
2020-09-06 11:57:48 +0300

    nested list inside the dictionary

--- Store_contact.py
+++ Store_contact.py
@@ -8,6 +8,10 @@ class User_Contact:
         self.Account_password = Account_password
 
 
+    def Account_credentials(self):
+        return '{} {} {}'.format(self.Account_name, self.User_name, self.Account_password)
+
+
 class User_credentials:
 
     def __init__(self, Account_name, User_name, Account_password):
@@ -15,8 +19,6 @@ class User_credentials:
         self.User_name  = User_name
         self.Account_password = Account_password
 
-    def Account_credentials(self):
-        return '{} {} {}'.format(self.Account_name, self.User_name, self.Account_password)
 
 print('Enter the Account name eg Twitter,Facebook, etc')
 Name_of_the_account = input()
@@ -27,8 +29,16 @@ Name_of_the_user  = input()
 print('Enter the accounts' 'password')
 User_password = input()
 
-user = User_credentials(Name_of_the_account, Name_of_the_user,User_password)
+list_of_Accounts = []
+Accounts_Dictionary = {}
 
+user = User_Contact(Name_of_the_account, Name_of_the_user,User_password)
+
+list_of_Accounts.append(user.Account_credentials())
+
+Accounts_Dictionary[Name_of_the_account] = list_of_Accounts
+
+print(Accounts_Dictionary)
 print(user.Account_credentials())
 
 
