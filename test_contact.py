1bb803f6fb7622f987f1bfa760a73d79e19abb2c
David Okwacha <osweredavid@gmail.com>
2020-09-18 14:59:45 +0300

    created the brother variable

--- test_contact.py
+++ test_contact.py
@@ -4,4 +4,5 @@ from Store_contact import User_credentials, User_Contact
 class test_units(unittest.TestCase):
 
     def setUp(self):
-        self.account = 'Gurur'
+        self.account = 'Twitter'
+        self.uses = 'Brother'
